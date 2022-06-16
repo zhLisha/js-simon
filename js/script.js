c73c2c565eb4077ab8fcfa3cf49f05fd07238a9b
zhLisha <lisazhou1709@gmail.com>
2022-06-16 14:13:17 +0200

    created random numer + alert number to memorize

--- js/script.js
+++ js/script.js
@@ -5,3 +5,51 @@
 */
 
 
+// Dichiaro la funzione per generare numeri random per l'array randomNumberList
+const randomNumberList = randomNumberArray(1, 100, 5);
+console.log('Array numeri da memorizzare', randomNumberList);
+
+// Creare un alert con i numeri presenti nell'Array: randomNumberList
+alert(`Memorizza questi numeri: ${randomNumberList}`);
+
+
+
+
+
+
+
+
+
+
+
+
+/*************************************
+            UTILITY FUNCTION 
+**************************************/
+// Genero numeri random tra totNum a totNum
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// GENERAZIONE DI NUMERI (RANDOM)
+// listRandomBumber ----> array in cui verranno inseriti i numeri random
+// maxRange ----> variabile function: numeri random da 1 a maxNumber
+// pushare nell'array listRandomBumber solo se i numeri non sono gia' presenti finche' non raggiunge un totale di: 5 elementi (in questo caso numeri)
+function randomNumberArray(minNumber, maxNumber, totalNumberElement) {
+
+    // Lista array 16 bombe da riempire
+    const listRandomBumber = [];
+
+    // Ciclo per generare numeri bomba fino ad arrivare a 16 elementi non duplicati da inserire in listBombsNumber
+    while(listRandomBumber.length < totalNumberElement) {
+
+        // Dichiarata la funzione di generatore numeri random da-a
+        let maxRange = randomNumber(minNumber, maxNumber);
+    
+        // Push solo dei lvMaxNumber non presenti ancora in listBombsNumber
+        if(!listRandomBumber.includes(maxRange)) {
+            listRandomBumber.push(maxRange);
+        } 
+    }
+    return listRandomBumber;
+}
