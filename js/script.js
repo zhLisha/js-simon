d4495e96700842c233b8627dfe646bb694ad5ef1
zhLisha <lisazhou1709@gmail.com>
2022-06-16 13:50:05 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* 
+- Alert di 5 numeri casuali
+- Dopo 30sec dalla chiusura dell'alert l'utente deve: inserire uno alla volta i numeri generati dal precedente alert
+- Dopo il completamento, il software deve dichiarare quanti e quali dei numeri sono stati individuati giusti
+*/
+
+
